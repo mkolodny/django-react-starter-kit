b3ba0c1fb6be7da78758a7d8b70cf5c2d3a2adef
Michael Kolodny <michaelckolodny@gmail.com>
2019-06-20 23:08:38 -0700

    Add react router

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
+import Home from '../Home';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/Home/Home.tsx</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
