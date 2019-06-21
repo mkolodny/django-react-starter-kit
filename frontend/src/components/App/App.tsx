0f4b64983d933f760a9d459601789b85ff04f1b1
Michael Kolodny <michaelckolodny@gmail.com>
2019-06-20 22:52:18 -0700

    Set up redux

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/logo.svg';
+import logo from '../../images/logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
