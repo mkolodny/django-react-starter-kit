12bc98041f5512949cafa54d1d07b74681464d61
Michael Kolodny <michaelckolodny@gmail.com>
2019-06-22 12:05:20 -0700

    Add django rest framework

--- backend/urls.py
+++ backend/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import include, path, re_path
+from rest_framework import routers
 
 from backend.app.views import FrontendAppView
 
 
+router = routers.SimpleRouter()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     re_path(r'^', FrontendAppView.as_view(), name='frontend-app'),
 ]
