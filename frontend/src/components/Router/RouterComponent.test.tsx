e12aa65c0342055d799c80017e136c4274d1f0db
Michael Kolodny <michaelckolodny@gmail.com>
2019-06-26 18:01:26 -0700

    Update router component import in test

--- frontend/src/components/Router/RouterComponent.test.tsx
+++ frontend/src/components/Router/RouterComponent.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from './App';
+import Router from '.';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<Router />);
 });
